920e8c67531c79e28a5f646e8b63069d26680f0c
Felipe <feferreira.sf@gmail.com>
2023-01-26 20:56:31 -0300

    feat: chats cards

---
+++ src/components/Chats.tsx
@@ -0,0 +1,26 @@
+import { VscCircleFilled } from 'react-icons/vsc'
+
+interface ChatsProps {
+  uid: number
+  displayName: string
+  lastMessage: string
+  photoURL: string
+}
+
+const Chats = ({ displayName, lastMessage, photoURL }: ChatsProps) => {
+  return (
+    <div className='flex gap-x-4 items-center mt-4'>
+      <div className='w-12 h-12 rounded-full overflow-hidden'>
+        <img src={photoURL} alt='' className='w-full h-full object-cover' />
+      </div>
+      <div>
+        <h2 className='font-bold'>{displayName}</h2>
+        <p className='text-xs flex items-center text-paragraph/40'>
+          {lastMessage}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Chats
