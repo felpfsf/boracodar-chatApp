ed5c92c5efc1d90d8b91ae785c20fe103f4c3188
Felipe <feferreira.sf@gmail.com>
2023-01-28 12:32:48 -0300

    feat: added file name on input file label

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useFormContext } from 'react-hook-form'
 import clsx from 'clsx'
 import { FcAddImage } from 'react-icons/fc'
@@ -20,15 +21,25 @@ const Input = ({ type, label, placeholder, name }: InputProps) => {
     formState: { errors }
   } = useFormContext()
 
+  const [fileName, setFileName] = useState('')
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFileName(e.target.files[0].name)
+    }
+  }
+
   return (
-    <div className='mb-6 relative'>
+    <div className='mb-8 relative'>
       {type === 'file' ? (
         <>
           <label
             htmlFor='photoURL'
             className='w-full flex items-end gap-x-4 cursor-pointer'>
             <FcAddImage className='w-14 h-14' />
-            <span className='text-paragraph'>Add an avatar</span>
+            <span className='text-paragraph'>
+              {fileName ? fileName : 'Add an avatar'}
+            </span>
           </label>
         </>
       ) : (
@@ -39,9 +50,10 @@ const Input = ({ type, label, placeholder, name }: InputProps) => {
         name={name}
         type={type}
         id={name}
+        onChange={handleChangeInput}
         className={clsx(
           {
-            'w-full px-1 block bg-transparent border-0 border-b-2 border-indigo-800 mt-0 focus:ring-0 focus:border-b-indigo-500 transition-colors duration-300 ease-in-out placeholder:text-paragraph/50':
+            'w-full px-1 block bg-transparent border-0 border-b-2 border-violet-800 mt-0 focus:ring-0 focus:border-b-violet-500 transition-colors duration-300 ease-in-out placeholder:text-paragraph/50 outline-none':
               type != 'file'
           },
           { hidden: type === 'file' }
@@ -50,7 +62,7 @@ const Input = ({ type, label, placeholder, name }: InputProps) => {
         accept='image/png, image/jpeg, image/jpg'
       />
       {errors[name]?.message && (
-        <div className='absolute flex items-center gap-2 text-red-500 font-semibold text-sm'>
+        <div className='absolute mt-1 flex items-center gap-2 text-red-500 font-light text-sm'>
           <FiAlertCircle />
           <span>{errors[name]?.message as string}</span>
         </div>
