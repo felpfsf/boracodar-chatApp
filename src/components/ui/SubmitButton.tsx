1c63f61453b6e77e1f2a9f57a9c2cc6746d498c9
Felipe <feferreira.sf@gmail.com>
2023-01-31 12:55:01 -0300

    feat: reusable submit button with variants

--- src/components/ui/SubmitButton.tsx
+++ src/components/ui/SubmitButton.tsx
@@ -1,6 +1,30 @@
-const SubmitButton = ({ label }: { label: string }) => {
+import clsx from 'clsx'
+import { CgSpinner } from 'react-icons/cg'
+
+const ButtonType = ['submit', 'loading'] as const
+
+type TButtonType = typeof ButtonType[number]
+
+interface ButtonProps {
+  disbale?: boolean
+  variant: TButtonType
+  label: string
+}
+
+const SubmitButton = ({ disbale, label, variant }: ButtonProps) => {
   return (
-    <button className='w-full border border-violet-500 py-2 bg-inputBg rounded-full mt-2'>
+    <button
+      className={clsx(
+        {
+          'w-full border border-violet-500 py-2 bg-inputBg rounded-full mt-2 flex items-center justify-center gap-4':
+            label != 'loading'
+        },
+        { 'cursor-not-allowed': variant === 'loading' && disbale }
+      )}
+      disabled={disbale}>
+      {variant === 'loading' ? (
+        <CgSpinner className='w-5 h-5 animate-spin' />
+      ) : null}
       {label}
     </button>
   )
