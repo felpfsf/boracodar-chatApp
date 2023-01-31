3efc6c4554b030acb88aa8f0f6bdb02acf170c99
Felipe <feferreira.sf@gmail.com>
2023-01-31 17:14:26 -0300

    feat: added displayName and photo of curr user

--- src/components/SidebarHeader.tsx
+++ src/components/SidebarHeader.tsx
@@ -1,20 +1,34 @@
+import { useAuthStore } from '../context/useAuthStore'
+
+interface UserProps {
+  displayName: string
+  photoURL: string
+}
 
 const SidebarHeader = () => {
+  const { logout, currentUser } = useAuthStore()
+  const { displayName, photoURL } = currentUser as UserProps
+
+  const handleSignOut = () => {
+    logout()
+  }
   return (
     <div className='flex flex-row-reverse xl:flex-row items-center justify-between pb-4 border-b border-b-paragraph/40 xl:flex-1'>
       {/* Perfil */}
       <div className='flex gap-x-4 items-center justify-center'>
         <div className='w-12 h-12 rounded-full overflow-hidden'>
           <img
-            src='/images/55.jpg'
+            src={photoURL}
             alt=''
             className='w-full h-full object-cover'
           />
         </div>
         <div>
-          <h2 className='font-bold'>Xavier Mohead</h2>
+          <h2 className='font-bold'>{displayName}</h2>
         </div>
-        <button className='border px-2'>Logout</button>
+        <button className='border px-2' onClick={handleSignOut}>
+          Logout
+        </button>
       </div>
     </div>
   )
