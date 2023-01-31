899ab3eb61a5b7661dffec534cc5fd44e3b78d4e
Felipe <feferreira.sf@gmail.com>
2023-01-31 17:14:41 -0300

    feat: protected route component

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import { useAuthStore } from '../context/useAuthStore'
+import Login from '../pages/Login'
+
+const ProtectedRoute = ({ element, ...rest }: { element: JSX.Element }) => {
+  const { currentUser } = useAuthStore()
+  // console.log(currentUser)
+
+  return (
+    <Routes {...rest}>
+      {!currentUser ? (
+        <Route path='*' element={<Login />} />
+      ) : (
+        <Route path='/*' element={element} />
+      )}
+    </Routes>
+  )
+}
+
+export default ProtectedRoute
